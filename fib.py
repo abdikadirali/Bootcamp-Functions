00a6c402512f01d1f1fb3af07fac0c24ecb29fff
abdikadirali <abdikadir@protonmail.com>
2016-10-09 20:39:30 +0300

    Update fib.py

--- fib.py
+++ fib.py
@@ -1,6 +1,11 @@
-def fibonacci(start=(0, 1), stop=FIB_STOP):
-    a, b = start
-    while stop:
-        yield a
-        a, b = b, a + b
-        stop -= 1
+def fib1(n):
+    """nth fibonacci number"""
+    a = 0
+    b = 1
+
+    if n == 0: 
+    	return a
+    if n == 1: 
+    	return b
+
+    return fib1(n-1) + fib1(n-2)
